b83e823e8da65cc11a177704b7616e8f7ebb7b0f
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-06-04 12:57:30 -0700

    Update `main.jsx` to use relative path for importing `App.css`

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage';
-import "/Users/new/Downloads/Projects/@latest/src/App.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
